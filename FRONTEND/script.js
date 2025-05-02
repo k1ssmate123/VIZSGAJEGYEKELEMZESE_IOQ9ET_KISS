929b795f5bfc6637bd8e0666c72aeb0279918ec4
k1ssmate123 <kissmate001@gmail.com>
2025-05-02 15:05:37 +0200

    delete button working, stats button added

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -9,18 +9,63 @@ async function downloadAndDisplay() {
         let tr = document.createElement('tr')
         let tdName = document.createElement('td')
         let tdMarks = document.createElement('td')
+        let tdBttns = document.createElement('td')
        
-
-        
         tdName.innerHTML = x.subjectName
         tdMarks.innerHTML = x.marks
-    
+
+        let btnStats = document.createElement('button')
+        btnStats.classList.add('btn')
+        btnStats.classList.add('btn-sm')
+        btnStats.classList.add('btn-warning')
+        btnStats.classList.add('mx-2')
+        btnStats.innerHTML = 'Statisztikák'
+        btnStats.idParameter = x.id
+        btnStats.addEventListener('click', showStats)
+        tdBttns.appendChild(btnStats)  
+        
+
+
+        let btnDel = document.createElement('button')
+        btnDel.classList.add('btn')
+        btnDel.classList.add('btn-sm')
+        btnDel.classList.add('btn-error')
+        btnDel.classList.add('mx-2')
+        btnDel.innerHTML = 'X'
+        btnDel.idParameter = x.id
+        btnDel.addEventListener('click', deleteSubject)
+        tdBttns.appendChild(btnDel)  
+        
+
+
         tr.appendChild(tdName)
         tr.appendChild(tdMarks)
+        tr.appendChild(tdBttns)
         document.querySelector('#tb').appendChild(tr)
     })
 }
 
+async function showStats(event)
+{
+    const response = await fetch('http://localhost:5186/ExamMarks/stats/'+event.target.idParameter)
+    stats = await response.json()
+}
+
+
+function deleteSubject(event)
+{
+    fetch('http://localhost:5186/ExamMarks/' + event.target.idParameter, {
+        method: 'DELETE',
+        headers: { 'Content-Type' : 'application/json', },
+        body: null
+    })
+    .then(resp => {
+        console.log('Response: ', resp)
+        if (resp.status === 200) {
+            downloadAndDisplay()
+        }
+    })
+}
 
 function addSubject()
 {
